b51e8b9db2a011b3dacd3c6c3a3196a8f0dab41a
docc46 <docc46@gmail.com>
2020-03-24 14:27:21 +0100

    Add component files

---
+++ campingList/campingListController.js
@@ -0,0 +1,28 @@
+({
+    clickCreateItem: function(component, event, helper) {
+        var validItem = component.find('itemform').reduce(function (validSoFar, inputCmp) {
+            // Displays error messages for invalid fields
+            inputCmp.showHelpMessageIfInvalid();
+            return validSoFar && inputCmp.get('v.validity').valid;
+        }, true);
+        // If we pass error checking, do some real work
+        if(validItem){
+            // Create the new Item
+            var newItem = component.get("v.newItem");
+            console.log("Create item: " + JSON.stringify(newItem));
+            var items = component.get("v.items");
+ 
+            // Copy the expense to a new object
+            // THIS IS A DISGUSTING, TEMPORARY HACK
+            var newItemToPush = JSON.parse(JSON.stringify(newItem));
+            
+            var newItems = JSON.parse(JSON.stringify(items));
+
+            newItems.push(newItemToPush);
+            component.set("v.items", newItems);
+            var blankitem = '{ \'sobjectType\': \'Camping_Item__c\', \'Name\' : \'\', \'Packed__c\' : false, \'Price__c\': 0, \'Quantity__c\': 0 }';
+            component.set("v.newItem",blankitem);
+
+        }
+    }
+})
